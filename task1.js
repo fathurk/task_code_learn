ea3a40479a7443a7d9c45d709f585fa9e7f7953a
fathurkaryim <fathurkaryim@gmail.com>
2022-08-03 22:06:36 +0700

    init task1

---
+++ task1.js
@@ -0,0 +1,18 @@
+// Buatlah sebuah script seperti kriteria dibawah
+// Buat 3 variabel dengan nama
+// - var a
+// - var b
+// - var c
+
+// Diasumsikan jumlah soal sebanyak 50
+// var a digunakan untuk menginput nama siswa
+// var b digunakan untuk menginput jumlah jawaban benar
+// var c digunakan untuk menyimpan nilai
+// lalu tampilkan menggunakan console log
+
+// ex.
+// var a = 'Jaki'
+// var b = 20
+// maka variabel c akan menyimpan nilai 20/50 yaitu var c = 40
+// lalu output akan seperti
+// output =>>>>  'Jaki - nilai - 40'
